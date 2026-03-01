4285e60a4e2f180919137f2998401d0087ebf662
Aryan <abhishekaanand@islingtoncollege.edu.com.np>
2026-03-02 02:10:02 +0545

    Refactoring IActionResult for better readability in responses from controllers

--- server/Movie.PresentationLayer/Controller/AuthController.cs
+++ server/Movie.PresentationLayer/Controller/AuthController.cs
@@ -16,41 +16,27 @@ public class AuthController : ControllerBase
     }
 
     [HttpGet("/checkLogin")]
-    public async Task<APIResponse<IActionResult>> GetToken()
+    public async Task<IActionResult> GetToken()
     {
         var tokens = HttpContext.Request.Headers.Authorization.ToString();
-        if(string.IsNullOrEmpty(tokens))
-        {
-            return new ()
-            {
-                Success = false,
-                Message = "Token not provided. Please login again"
-            };
-        }
 
-        return new ()
+        return Ok(new APIResponse<string>
         {
             Success = true,
-            Message = "Yep you are logged in"
-        };
+            Message = "Yep you are logged in",
+            Data = tokens
+        });
     }
 
     [HttpGet("/getUsers")]
-    public async Task<APIResponse<User>> GetUsers(string userID)
+    public async Task<IActionResult> GetUsers(string userID)
     {
-        try
+        var getUser = await _userService.GetUserById(userID);
+        return Ok(new APIResponse<User>
         {
-            var getUser = await _userService.GetUserById(userID);
-            return new ()
-            {
-                Success = true,
-                Message = "User Fetched",
-                Data = getUser
-            };
-        }
-        catch (Exception ex)
-        {
-            throw new Exception(ex.Message);
-        }
+            Success = true,
+            Message = "User Fetched",
+            Data = getUser
+        });
     }
 }
