e69e32db3d78f17b2b54cf8962f56e0310c8cab5
Aryan <abhishekaanand@islingtoncollege.edu.com.np>
2026-03-03 17:35:58 +0545

    Adding Delete Movie Method

--- server/Movie.ApplicationLayer/Interface/FilmInterface/IFilmInterface.cs
+++ server/Movie.ApplicationLayer/Interface/FilmInterface/IFilmInterface.cs
@@ -8,10 +8,10 @@ namespace Movie.Application.Interface.FilmInterface;
 public interface IFilmInterface
 {
     Task<List<AddFilmDTO>> GetFilmsAsync();
-    Task<Film> FilterByGenre(Genre genre);
-    Task<Film> FilterByReleaseYear(string year);
     Task<Film> ShowMovieDetails(Guid movieId);
     Task<Film> AddMovieDetails(AddFilmDTO filmDTO);
     Task<int> GetTotalRatings(Guid movieId);
     Task<Film> UpdateFilm(Guid movieid, UpdateFilmDTO updateFilmDTO);
+    Task<List<Film>> SearchMovie(string title);
+    Task DeleteMovie(Guid movieId);
 }
